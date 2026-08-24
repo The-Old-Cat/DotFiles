2f61468c3d5c39d3047614ca353c87b786df2d09
The-Old-Cat <artkov476@gmail.com>
2026-08-24 14:56:29 +0300

    modified: private_dot_config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,17 +1,22 @@
 local wezterm = require 'wezterm'
 local c = wezterm.config_builder()
 
--- Шрифт + лигатуры
-c.font = wezterm.font_with_fallback { 'Hasklug Nerd Font Mono', 'Consolas' }
+-- CaskaydiaCove (лигатуры)
+c.font = wezterm.font_with_fallback {
+    'CaskaydiaCove Nerd Font',
+    'CaskaydiaCove Nerd Font Mono',
+    'Consolas',
+}
 c.font_size = 11.0
 c.harfbuzz_features = { 'calt', 'liga', 'dlig' }
 
--- Tokyo Night (встроенная схема)
 c.color_scheme = 'tokyonight'
 c.window_background_opacity = 0.92
 c.hide_tab_bar_if_only_one_tab = true
 
--- ОС-ветвление без chezmoi-шаблонов
+local home = os.getenv('USERPROFILE') or os.getenv('HOME')
+c.font_dirs = { home .. '\\scoop\\apps\\Cascadia-Code\\current' }
+
 if wezterm.target_triple:find('windows') then
     c.default_prog = { 'pwsh.exe', '--nologo' }
 else
