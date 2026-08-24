0ea9983beeae7897a54434a94cadf4cba98f54a2
The-Old-Cat <artkov476@gmail.com>
2026-08-24 15:28:49 +0300

    private_dot_config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,3 +1,33 @@
+local wezterm = require 'wezterm'
+local c = wezterm.config_builder()
+
+-- --- Шрифт (JetBrainsMono Nerd Font) ---
+c.font = wezterm.font_with_fallback {
+    'JetBrainsMono Nerd Font Mono',
+    'JetBrainsMono Nerd Font',
+    'CaskaydiaCove Nerd Font Mono',
+    'Consolas',
+}
+c.font_size = 11.0
+c.harfbuzz_features = { 'calt', 'liga', 'dlig' }
+
+-- --- Цветовая схема ---
+c.color_scheme = 'Tokyo Night'
+
+-- --- Окно ---
+c.window_background_opacity = 0.92
+c.window_decorations = 'RESIZE'
+c.window_padding = {
+    left = 10,
+    right = 10,
+    top = 10,
+    bottom = 10,
+}
+
+-- --- Табы ---
+c.hide_tab_bar_if_only_one_tab = true
+c.enable_tab_bar = false
+
 -- --- Клавиатура ---
 c.keys = {
     -- Копировать (Ctrl+C / Ctrl+Insert) — работа и в RU, и в EN
@@ -31,3 +61,18 @@ c.keys = {
     { key = '0', mods = 'CTRL', action = wezterm.action.ResetFontSize },
     { key = '0', mods = 'CTRL|SHIFT', action = wezterm.action.ResetFontSize },
 }
+-- --- Мышь ---
+c.mouse_bindings = {
+    -- Правый клик = вставка
+    { event = { Down = { streak = 1, button = 'Right' } },
+      action = wezterm.action.PasteFrom 'Clipboard' },
+}
+
+-- --- Оболочка ---
+if wezterm.target_triple:find('windows') then
+    c.default_prog = { 'pwsh.exe', '-NoExit' }
+else
+    c.default_prog = { '/bin/bash', '--login' }
+end
+
+return c
